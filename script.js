f0fedfac5718195803f5893616633844b5bc647d
ptaich <47002264+ptaich@users.noreply.github.com>
2019-05-02 00:50:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,198 @@
+$(document).ready(function () {
+$(".circle1").mouseenter(function () {
+$(".monarchtext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle1").mouseleave(function () {
+$(".monarchtext").removeClass("show");
+});
+})
+
+
+$(document).ready(function () {
+$(".circle2").mouseenter(function () {
+$(".foxtext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle2").mouseleave(function () {
+$(".foxtext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle3").mouseenter(function () {
+$(".salamandertext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle3").mouseleave(function () {
+$(".salamandertext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle4").mouseenter(function () {
+$(".cranetext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle4").mouseleave(function () {
+$(".cranetext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle6").mouseenter(function () {
+$(".belugatext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle6").mouseleave(function () {
+$(".belugatext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle5").mouseenter(function () {
+$(".penguintext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle5").mouseleave(function () {
+$(".penguintext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle7").mouseenter(function () {
+$(".gulltext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle7").mouseleave(function () {
+$(".gulltext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle8").mouseenter(function () {
+$(".koalatext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle8").mouseleave(function () {
+$(".koalatext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle9").mouseenter(function () {
+$(".polartext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle9").mouseleave(function () {
+$(".polartext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle10").mouseenter(function () {
+$(".turtletext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle10").mouseleave(function () {
+$(".turtletext").removeClass("show");
+});
+})
+
+
+
+$(document).ready(function () {
+$(".circle11").mouseenter(function () {
+$(".pikatext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle11").mouseleave(function () {
+$(".pikatext").removeClass("show");
+});
+})
+
+
+$(document).ready(function () {
+$(".circle12").mouseenter(function () {
+$(".beetext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle12").mouseleave(function () {
+$(".beetext").removeClass("show");
+});
+})
+
+
+$(document).ready(function () {
+$(".circle13").mouseenter(function () {
+$(".clownfishtext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle13").mouseleave(function () {
+$(".clownfishtext").removeClass("show");
+});
+})
+
+
+$(document).ready(function () {
+$(".circle14").mouseenter(function () {
+$(".turtle2text").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle14").mouseleave(function () {
+$(".turtle2text").removeClass("show");
+});
+})
+
+
+$(document).ready(function () {
+$(".circle15").mouseenter(function () {
+$(".sealtext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle15").mouseleave(function () {
+$(".sealtext").removeClass("show");
+});
+})
+
+$(document).ready(function () {
+$(".circle16").mouseenter(function () {
+$(".thrushtext").addClass("show");
+});
+})
+$(document).ready(function () {
+$(".circle16").mouseleave(function () {
+$(".thrushtext").removeClass("show");
+});
+})
